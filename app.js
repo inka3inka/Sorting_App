8b6be252b92e0edc868697e4cd9f82ada669d4d0
inka3inka <inka3inka@gmail.com>
2019-09-12 22:15:32 +0200

    sorting

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
-console.log('Hello word');
+document.addEventListener('DOMContentLoaded', function(){
+
+    let buttonSort = document.querySelector('.btn-sorting');
+    let buttonShuffle = document.querySelector('.btn-shuffle');
+    let buttonRestore = document.querySelector('.btn-restore');
+    let buttonClear = document.querySelector('.btn-clear');
+    let area = document.querySelector('.area');
+
+
+    buttonSort.addEventListener('click', function(){
+        area.innerText = area.value;
+        let arrayOfStrings = area.value.split(/\r\n|\n|\r/);
+        let sortedArray = arrayOfStrings.sort();
+        area.value = sortedArray.join("\n");
+
+    });
+
+    buttonClear.addEventListener('click', function(){
+        area.value = '';
+    })
+
+});
