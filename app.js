7f1c8c4b90e3f074388ed4e5e88990ab2a7f4b97
inka3inka <inka3inka@gmail.com>
2019-09-12 23:55:51 +0200

    shuffle

--- app.js
+++ app.js
@@ -12,7 +12,22 @@ document.addEventListener('DOMContentLoaded', function(){
         let arrayOfStrings = area.value.split(/\r\n|\n|\r/);
         let sortedArray = arrayOfStrings.sort();
         area.value = sortedArray.join("\n");
+    });
 
+    buttonShuffle.addEventListener('click', function(){
+        area.innerText = area.value;
+        let arrayOfStrings = area.value.split(/\r\n|\n|\r/);
+        function shuffler(array) {
+            for (let i = 0; i < array.length; i++) {
+                let j = Math.floor(Math.random() * (array.length - 1));
+                let x = array[i];
+                array[i] = array[j];
+                array[j] = x;
+            }
+            return array;
+        }
+        console.log(shuffler(arrayOfStrings));
+        area.value = arrayOfStrings.join("\n");
     });
 
     buttonClear.addEventListener('click', function(){
@@ -20,3 +35,4 @@ document.addEventListener('DOMContentLoaded', function(){
     })
 
 });
+
